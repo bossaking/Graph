3349cf270a618a8d8931c8cf86e3c9a47406d235
Greezzy <ddgamescompany@gmail.com>
2020-05-28 19:44:14 +0200

    Code cleared

--- Graph/Assets/TestLine.cs
+++ Graph/Assets/TestLine.cs
@@ -15,35 +15,40 @@ public class TestLine : MonoBehaviour
     private List<GameObject> inputNodes = new List<GameObject>();
     private List<GameObject> weightNodes = new List<GameObject>();
 
+
     private LineRenderer lineRenderer;
 
     public int nodesCount;
 
     public void Start()
     {
-        CreateInputNodes();
-    }
-
-    private void CreateInputNodes()
-    {
-
         float centerY = 0;
         float partCenter = Screen.height / nodesCount / 2;
         float parts = Screen.height / nodesCount;
 
+        CreateInputNodes(centerY, partCenter, parts);
+        CreateWeightNodes(centerY, partCenter, parts);
+        CreateOutputNode();
+    }
+
+    private void CreateInputNodes(float centerY, float partCenter, float parts)
+    {
+
         for (int i = 0; i < nodesCount; i++)
         {
             inputNodes.Add(Instantiate(inputNodePrefab, parentCanvas.transform));
-
             centerY = partCenter + i * parts;
-
             inputNodes[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(100f, -centerY);
         }
+ 
+    }
 
-        partCenter = (inputNodes[0].GetComponent<RectTransform>().anchoredPosition.y - 
+    private void CreateWeightNodes(float centerY, float partCenter, float parts)
+    {
+        partCenter = (inputNodes[0].GetComponent<RectTransform>().anchoredPosition.y -
             inputNodes[inputNodes.Count - 1].GetComponent<RectTransform>().anchoredPosition.y) / nodesCount / 2;
 
-        parts = (inputNodes[0].GetComponent<RectTransform>().anchoredPosition.y - 
+        parts = (inputNodes[0].GetComponent<RectTransform>().anchoredPosition.y -
             inputNodes[inputNodes.Count - 1].GetComponent<RectTransform>().anchoredPosition.y) / nodesCount;
 
         for (int i = 0; i < nodesCount; i++)
@@ -56,7 +61,10 @@ public class TestLine : MonoBehaviour
 
             DrawLine(new Transform[] { inputNodes[i].transform, weightNodes[i].transform });
         }
+    }
 
+    private void CreateOutputNode()
+    {
         GameObject outputNode = Instantiate(outputNodePrefab, parentCanvas.transform);
         outputNode.GetComponent<RectTransform>().anchoredPosition = new Vector2(600f, outputNode.GetComponent<RectTransform>().anchoredPosition.y);
 
@@ -69,7 +77,7 @@ public class TestLine : MonoBehaviour
     private void DrawLine(Transform[] transforms)
     {
         GameObject line = new GameObject();
-        
+
         lineRenderer = line.AddComponent<LineRenderer>();
         lineRenderer.startWidth = 0.1f;
         lineRenderer.endWidth = 0.1f;
@@ -79,7 +87,7 @@ public class TestLine : MonoBehaviour
 
         Vector3[] pointsArray = new Vector3[2];
 
-        for(int i = 0; i < 2; i++)
+        for (int i = 0; i < 2; i++)
         {
             Vector3 pointPos = transforms[i].position;
             pointsArray[i] = new Vector3(pointPos.x, pointPos.y, 0f);
